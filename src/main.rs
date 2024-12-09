40b3947483081fe6aef0843fa43bc531eb56001f
roscoes-wetsuit <0xcost@gmail.com>
2024-12-08 19:48:06 -0500

    Create program w/ shaders

--- src/main.rs
+++ src/main.rs
@@ -7,13 +7,14 @@ use glium::{
         event_loop::{ActiveEventLoop, EventLoop},
         window::{Window, WindowId},
     },
-    Display, Surface,
+    Display, Program, Surface,
 };
 
 #[derive(Default)]
 struct App {
     window: Option<Window>,
     display: Option<Display<WindowSurface>>,
+    program: Option<Program>,
 }
 
 impl ApplicationHandler for App {
@@ -23,8 +24,68 @@ impl ApplicationHandler for App {
             .with_inner_size(2560, 1600)
             .build(event_loop);
 
+        let vertex_shader_src = r#"
+            #version 410 core
+
+            in vec4 coord;
+            in vec4 color;
+
+            out vec2 texpos;
+            out vec4 colormod;
+
+            uniform vec2 screensize;
+            uniform int yoffset;
+
+            void main(void)
+            {
+                float x = -1.0 + coord.x * 2.0 / screensize.x;
+                float y = 1.0 - (coord.y + float(yoffset)) * 2.0 / screensize.y;
+                gl_Position = vec4(x, y, 0.0, 1.0);
+                texpos = coord.zw;
+                colormod = color;
+            }
+        "#;
+
+        let fragment_shader_src = r#"
+            #version 410 core
+
+		    in vec2 texpos;
+            in vec4 colormod;
+
+            uniform sampler2D tex;
+            uniform vec2 atlassize;
+            uniform int fontregion;
+
+            out vec4 FragColor;
+
+            void main(void)
+            {
+                if (texpos.y == 0)
+                {
+                    FragColor = colormod;
+                }
+                else if (texpos.y <= float(fontregion))
+                {
+                    FragColor = vec4(1, 1, 1, texture(tex, texpos / atlassize).r) * colormod;
+                }
+                else
+                {
+                    FragColor = texture(tex, texpos / atlassize) * colormod;
+                }
+            }
+        "#;
+
         self.window = Some(window);
         self.display = Some(display);
+        self.program = Some(
+            Program::from_source(
+                self.display.as_ref().unwrap(),
+                vertex_shader_src,
+                fragment_shader_src,
+                None,
+            )
+            .unwrap(),
+        );
     }
 
     fn window_event(&mut self, event_loop: &ActiveEventLoop, id: WindowId, event: WindowEvent) {
