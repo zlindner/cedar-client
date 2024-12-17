fc2408b06f881dd04212a6a7160f6fd3e31001bb
zlindner <zach.lindner@hotmail.com>
2024-12-16 20:39:26 -0500

    Create a render pipeline

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use std::sync::Arc;
 
+use wgpu::include_wgsl;
 use winit::{
     application::ApplicationHandler,
     dpi::PhysicalSize,
@@ -109,6 +110,12 @@ impl ApplicationHandler for AppState {
             WindowEvent::Resized(new_size) => {
                 app.resize(new_size);
             }
+            WindowEvent::CursorMoved {
+                position: new_position,
+                ..
+            } => {
+                // TODO: we should call some `cursor.update(new_position)` here.
+            }
             _ => (),
         }
     }
@@ -121,6 +128,7 @@ struct Gpu {
     device: wgpu::Device,
     queue: wgpu::Queue,
     surface_config: wgpu::SurfaceConfiguration,
+    render_pipeline: wgpu::RenderPipeline,
 }
 
 impl Gpu {
@@ -163,6 +171,56 @@ impl Gpu {
 
         surface.configure(&device, &surface_config);
 
+        let shader = device.create_shader_module(include_wgsl!("shader.wgsl"));
+
+        let render_pipeline_layout =
+            device.create_pipeline_layout(&wgpu::PipelineLayoutDescriptor {
+                label: Some("Render Pipeline Layout"),
+                bind_group_layouts: &[],
+                push_constant_ranges: &[],
+            });
+
+        let render_pipeline = device.create_render_pipeline(&wgpu::RenderPipelineDescriptor {
+            label: Some("Render Pipeline"),
+            layout: Some(&render_pipeline_layout),
+            vertex: wgpu::VertexState {
+                module: &shader,
+                entry_point: Some("vs_main"),
+                buffers: &[],
+                compilation_options: wgpu::PipelineCompilationOptions::default(),
+            },
+            fragment: Some(wgpu::FragmentState {
+                module: &shader,
+                entry_point: Some("fs_main"),
+                targets: &[Some(wgpu::ColorTargetState {
+                    format: surface_config.format,
+                    blend: Some(wgpu::BlendState::REPLACE),
+                    write_mask: wgpu::ColorWrites::ALL,
+                })],
+                compilation_options: wgpu::PipelineCompilationOptions::default(),
+            }),
+            primitive: wgpu::PrimitiveState {
+                topology: wgpu::PrimitiveTopology::TriangleList,
+                strip_index_format: None,
+                front_face: wgpu::FrontFace::Ccw,
+                cull_mode: Some(wgpu::Face::Back),
+                // Setting this to anything other than Fill requires Features::NON_FILL_POLYGON_MODE.
+                polygon_mode: wgpu::PolygonMode::Fill,
+                // Requires Features::DEPTH_CLIP_CONTROL.
+                unclipped_depth: false,
+                // Requires Features::CONSERVATIVE_RASTERIZATION.
+                conservative: false,
+            },
+            depth_stencil: None,
+            multisample: wgpu::MultisampleState {
+                count: 1,
+                mask: !0,
+                alpha_to_coverage_enabled: false,
+            },
+            multiview: None,
+            cache: None,
+        });
+
         Self {
             instance,
             surface,
@@ -170,6 +228,7 @@ impl Gpu {
             device,
             queue,
             surface_config,
+            render_pipeline,
         }
     }
 
@@ -186,7 +245,7 @@ impl Gpu {
                 label: Some("Render Encoder"),
             });
 
-        let render_pass = encoder.begin_render_pass(&wgpu::RenderPassDescriptor {
+        let mut render_pass = encoder.begin_render_pass(&wgpu::RenderPassDescriptor {
             label: Some("Render Pass"),
             color_attachments: &[Some(wgpu::RenderPassColorAttachment {
                 view: &view,
@@ -205,6 +264,9 @@ impl Gpu {
             occlusion_query_set: None,
             timestamp_writes: None,
         });
+
+        render_pass.set_pipeline(&self.render_pipeline);
+        render_pass.draw(0..3, 0..1);
 
         // The render pass must be dropped before calling `encoder.finish()`.
         drop(render_pass);
